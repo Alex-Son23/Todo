480fb0b0d7f43f112631dd0f0abd4b5d2045158e
Alex <alexson2302@gmail.com>
2022-08-16 21:34:14 +0300

    lesson-4 homework

--- todo/authapp/views.py
+++ todo/authapp/views.py
@@ -1,7 +1,8 @@
+from rest_framework import mixins
 from django.shortcuts import render
 from djangorestframework_camel_case.parser import CamelCaseJSONParser
 from rest_framework import parsers
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from authapp.models import TodoUser
 from authapp.serializers import TodoUserModelSerializer
 
@@ -11,8 +12,12 @@ class NoUnderscoreBeforeNumberCamelCaseJSONParser(CamelCaseJSONParser):
     json_underscoreize = {'no_underscore_before_number': True}
 
 
-class TodoUserModelViewSet(ModelViewSet):
+# class TodoUserModelViewSet(ModelViewSet):
+#     queryset = TodoUser.objects.all()
+#     serializer_class = TodoUserModelSerializer
+#     parser_classes = (NoUnderscoreBeforeNumberCamelCaseJSONParser, parsers.MultiPartParser, parsers.JSONParser)
+
+class TodoUserCustomViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, GenericViewSet):
     queryset = TodoUser.objects.all()
     serializer_class = TodoUserModelSerializer
     parser_classes = (NoUnderscoreBeforeNumberCamelCaseJSONParser, parsers.MultiPartParser, parsers.JSONParser)
-
