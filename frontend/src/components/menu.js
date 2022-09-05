7dac62c69ed866cf7578aa062d25de495bd709b4
Alex <alexson2302@gmail.com>
2022-09-05 22:53:25 +0300

    lesson-7

--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -1,17 +1,24 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 
 const MenuItem = ({item}) => {
     return (
-        <li><a href='#' class='menu-but'>{item.name}</a></li>
+        <li>
+            <Link to={item.href} className={'menu-but'}>{item.name}</Link>
+        </li>
     )
 }
 
-const MenuList = ({items}) => {
+const MenuList = ({items, auth, logout}) => {
     return (
         <nav>
             <ul>
                 {items.map((item) => <MenuItem item={item}/>)}
+                {auth.isLogin ? 
+                <li><span className={'menu-but'}>{auth.username}</span> <button className={'menu-but'} onClick={logout}>Logout</button></li>
+                :
+                <li><Link to='/login' className={'menu-but'}>Login</Link></li>}
             </ul>
         </nav>
     )
